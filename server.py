1781f4678e585375743dbbf1ecaaafe35661adbc
darshan527 <darshan.hosamani6@gmail.com>
2021-04-24 15:50:29 +0530

    Added User table

--- server.py
+++ server.py
@@ -1,8 +1,35 @@
 from flask import Flask, jsonify, request
 from sqlite3 import Connection as SQLite3Connection
-from datetime import datetime
+from datetime import date, datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask_sqlalchemy import SQLAlchemy
+
+
+# configure sqlite3 to enforce foreign key constraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
 
 #main App
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
-app.config["SQL_TRACKMODIFICATION"] = 0
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = 0
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+
+# Models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(10))
+    posts = db.relationship("BlogPost")
